1173a3859c405da21f0803c11419cad3bbb30e37
azureglades <hegderutvik@gmail.com>
2026-01-20 21:50:55 +0530

    idk bro

--- talos/src-tauri/src/main.rs
+++ talos/src-tauri/src/main.rs
@@ -1,6 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![allow(non_snake_case)]
 
 fn main() {
     talos_lib::run()
